2640bb39f22093f3a9af5a6dc1699b1c889fb653
Lee Leahy <lpleahyjr@gmail.com>
2022-09-23 23:40:24 -1000

    Add SAMD_GPIO.ino
    Add routines to display the SAMD GPIO configuration

--- Firmware/LoRaSerial_Firmware/LoRaSerial_Firmware.ino
+++ Firmware/LoRaSerial_Firmware/LoRaSerial_Firmware.ino
@@ -490,6 +490,9 @@ void setup()
 
   updateRTS(true); //Enable serial input
 
+samdGpioPortConfig('A');
+samdGpioPortConfig('B');
+samdGpioPortConfig('C');
   systemPrintTimestamp();
   systemPrintln("LRS Setup Complete");
 
